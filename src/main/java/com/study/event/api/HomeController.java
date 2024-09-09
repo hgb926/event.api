84bf53eb33298e253d2cb8991a7d993476a57d0b
hgb926 <hgb926@naver.com>
2024-09-09 21:30:04 +0900

    0909 음성통신 구현 전

---
+++ src/main/java/com/study/event/api/HomeController.java
@@ -0,0 +1,13 @@
+package com.study.event.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "도커로 구동확인!!";
+    }
+}
--- src/main/java/com/study/event/api/config/SecurityConfig.java
+++ src/main/java/com/study/event/api/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
                 // 아래의 URL요청은 로그인 없이 모두 허용
                 .antMatchers("/", "/auth/**", "/file/**").permitAll()
 
-                // 위에선 auth/**는 다 풀어주지만! /auth/promote는 인증받고오라. COMMON만
+                // 위에선 auth/**는 다 풀어주지만 /auth/promote는 인증받고오라. COMMON만
                 .antMatchers(HttpMethod.PUT, "/auth/promote").hasAuthority("COMMON")
 
                 // 나머지 요청은 전부 인증(로그인) 후 진행해라
